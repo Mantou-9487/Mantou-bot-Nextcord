5d23b5ad50d81681af9ac47cc2f6c1cc0bd00de2
Mantou-9487 <opcantel@gmail.com>
2022-10-26 22:13:40 +0800

    Push Github1

--- keep_alive.py
+++ keep_alive.py
@@ -6,10 +6,3 @@ app = Flask('')
 @app.route('/')
 def main():
 	return 'Bot is aLive!'
-
-def run():
-    app.run(host="0.0.0.0", port=8080)
-
-def keep_alive():
-    server = Thread(target=run)
-    server.start()
