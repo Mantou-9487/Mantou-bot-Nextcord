1ce0a392649af216f9e07ab77fd7260aed89902a
Mantou-9487 <opcantel@gmail.com>
2022-12-01 18:03:44 +0800

    Push Github0

--- cogs/fight.py
+++ cogs/fight.py
@@ -250,7 +250,7 @@ class AttackView(nextcord.ui.View):
             trophyemoji = nextcord.utils.get(guild.emojis, name="trophy")
             view = OverView()
             print("遊戲結束!")
-            embed = nextcord.Embed(title="{0} | 遊戲結束!".format(trophyemoji),description="恭喜 {1} 勝利!!!!!!!!!".format(interaction.user.name), colour=int(color, 16))
+            embed = nextcord.Embed(title="{} | 遊戲結束!".format(trophyemoji),description="恭喜 {} 勝利!!!!!!!!!".format(interaction.user.name), colour=int(color, 16))
             await interaction.followup.edit_message(message_id=message_fetch.id ,embed=embed, view=view)
         elif self.victim_hp <=0:
             inte = int("FFA500", 16)
@@ -258,7 +258,7 @@ class AttackView(nextcord.ui.View):
             trophyemoji = nextcord.utils.get(guild.emojis, name="trophy")
             view = OverView()
             print("遊戲結束!")
-            embed = nextcord.Embed(title="{0} | 遊戲結束!".format(trophyemoji),description="恭喜 {1} 勝利!!!!!!!!!".format(interaction.user.name), colour=int(color,16))
+            embed = nextcord.Embed(title="{} | 遊戲結束!".format(trophyemoji),description="恭喜 {} 勝利!!!!!!!!!".format(interaction.user.name), colour=int(color,16))
             await interaction.followup.edit_message(message_id=message_fetch.id ,embed=embed, view=view)
 
 class OverView(nextcord.ui.View):
---
+++ cogs/info.py
@@ -0,0 +1,16 @@
+import nextcord
+from nextcord.ext import commands
+from nextcord import Interaction, Member, SlashOption
+from typing import Optional
+
+class Info(commands.Cog):
+    def __init__(self):
+        super().__init__()
+
+    @nextcord.slash_command(name="查看使用者資訊",description="透過ID查別人")
+    async def userinfo(self,interaction:Interaction, target:Optional[Member] = SlashOption(description="放你要查的人ID")):
+        embed = nextcord.Embed(name={target.name})
+        embed.add_field(name="用戶ID",value=f"{target.id}")
+        embed.add_field(name="正在玩",value=f"{target.activity.name} {str(getattr(target.activity, type)).title()}")
+        embed.add_field(name="帳號創立時間",value=target.created_at.strftime(f"%d/%m/%Y %H:%M:S"))
+        embed.set_thumbnail(url=target.avatar.url)
